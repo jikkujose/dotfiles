b1729ba5295115df92a541a94075da128489ea14
Jikku Jose <jikkujose@gmail.com>
2016-07-14 10:16:50 +0530

    Add bitcoind path

--- paths.zsh
+++ paths.zsh
@@ -1,5 +1,7 @@
 GOPATH="$HOME/Go"
 NODEPATH="/usr/local/Cellar/node/5.11.0/bin"
+BITCOIN_PATH="$HOME/bitcoin/bin"
 PATH="/usr/local/bin:$PATH"
 PATH="$HOME/commands:$PATH"
 PATH="$NODEPATH:$PATH"
+PATH="$BITCOIN_PATH:$PATH"
