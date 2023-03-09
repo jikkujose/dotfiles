abaa9e8c013b61a5abe452c9289e83325f49bbd4
Jikku Jose <jikku@storybrain.com>
2023-03-09 10:22:19 +0530

    Remove unwanted

--- functions.zsh
+++ functions.zsh
@@ -4,21 +4,6 @@ ip(){
   echo "WiFi    : `ifconfig | grep inet | grep broadc | cut -d ' ' -f 2`"
 }
 
-# change directory to your finders location
-function cdfinder()
-{
-cd "$(osascript -e 'tell application "Finder"' \
-  -e 'set myname to POSIX path of (target of window 1 as alias)' \
-  -e 'end tell' 2>/dev/null)"
-}
-
-if [ -z "$TMUX" ]; then
-  tmux attach-session -t "$target_session" \; select-pane -t "$target_window.$target_pane"
-else
-  tmux switch-client -t "$target_session:$target_window.$target_pane"
-fi
-  }
-
 take() {
     mkdir -p "$@" && cd "$@"
 }
