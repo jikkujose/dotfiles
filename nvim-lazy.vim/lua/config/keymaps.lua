aa1f27fbf2724fa7ab147dbe1adc8d56aae380c1
Jikku Jose <jikku@storybrain.com>
2023-03-15 22:32:26 +0530

    Add rufo

--- nvim-lazy.vim/lua/config/keymaps.lua
+++ nvim-lazy.vim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- map("n", , ":! rufo % <cr>", )
+vim.keymap.set("n", "<leader>r", ":! rufo % <CR>", { desc = "Format Ruby" })
