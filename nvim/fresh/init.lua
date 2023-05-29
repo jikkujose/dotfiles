8891f1bece51248e928253062117deb1048c4e5f
Jikku Jose <jikku@storybrain.com>
2023-05-29 09:52:55 +0530

    Start populating init.lua

--- nvim/fresh/init.lua
+++ nvim/fresh/init.lua
@@ -17,4 +17,39 @@ vim.cmd[[
   set hlsearch
   set ignorecase
   set nobackup nowritebackup
+
+  nmap gn :%s///gn<cr>
+  nmap gr :%s///g<cr>
+
+  function! RenameFile()
+    let old_name = expand('%')
+    let new_name = input('New file name: ', expand('%'), 'file')
+    if new_name != '' && new_name != old_name
+      exec ':saveas ' . new_name
+      exec ':silent !rm ' . old_name
+      redraw!
+    endif
+  endfunction
+
+  nmap gmv :call RenameFile()<cr>
+  nnoremap <leader><leader> <C-^>
+  nnoremap U <c-r>
+  map <silent> <Leader>c :TComment<CR>
+  noremap <leader>w :w<CR>
+  noremap , :set hlsearch! hlsearch?<CR>
+  noremap <leader>p :set paste<CR>:put  *<CR>:set nopaste<CR>
+  nmap <leader>f :e %:h/
+  nmap <silent> <leader>t :StripWhitespace<cr>
+  nmap <C-b> :CtrlPBuffer<CR>
+  :nnoremap K i<CR><Esc>
+
+  map <leader><up> <c-w><up>
+  map <leader><down> <c-w><down>
+  map <leader><left> <c-w><left>
+  map <leader><right> <c-w><right>
+
+call plug#begin('~/.fresh/plugged')
+  Plug 'neoclide/coc.nvim', {'branch': 'release'}
+call plug#end()
+
 ]]
