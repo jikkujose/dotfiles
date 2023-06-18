778f812e5d784f1cac1a526d6e83b128a5b91319
Jikku Jose <jikku@storybrain.com>
2023-06-18 14:29:47 +0530

    Remove conda

--- zshrc.zsh
+++ zshrc.zsh
@@ -41,22 +41,6 @@ fi
 
 export NODE_PATH=$(npm -g root)
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/whiterabbit/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/whiterabbit/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/whiterabbit/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/whiterabbit/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
-
 # bun completions
 [ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
 
