c5f697db58d45e97a88ee9e2aa2f6f8baaaa64eb
Jikku Jose <jikku@storybrain.com>
2023-06-18 15:06:13 +0530

    Add tool-versions

--- install.zsh
+++ install.zsh
@@ -2,3 +2,4 @@ mkdir ~/.config
 ln -s ~/dotfiles/zshrc.zsh ~/.zshrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/nvim/fresh ~/.config/nvim
+ln -s ~/dotfiles/tool-versions ~/.tool-versions
