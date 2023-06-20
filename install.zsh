f05e067fff72bfcb5e89b2088a05567d41085ccd
Jikku Jose <jikku@storybrain.com>
2023-06-20 20:17:16 +0530

    Split starship config

--- install.zsh
+++ install.zsh
@@ -1,6 +1,15 @@
+#!/usr/bin/env zsh
+
 mkdir -p ~/.config
+rm -rf ~/.config/nvim
+
 ln -sf ~/dotfiles/zshrc.zsh ~/.zshrc
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/nvim/fresh ~/.config/nvim
 ln -sf ~/dotfiles/tool-versions ~/.tool-versions
-ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
+
+if [[ "$IS_CONTAINER" == "yes"* ]]; then
+  ln -sf ~/dotfiles/starship/container.toml ~/.config/starship.toml
+else
+  ln -sf ~/dotfiles/starship/linux.toml ~/.config/starship.toml
+fi
