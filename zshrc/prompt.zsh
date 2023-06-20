64f7ee52387201392b0572031fc6acef3be7138d
Jikku Jose <jikku@storybrain.com>
2023-06-20 14:32:46 +0530

    Savepoint

--- zshrc/prompt.zsh
+++ zshrc/prompt.zsh
@@ -1,8 +1,16 @@
 autoload -Uz vcs_info
-zstyle ':vcs_info:git:*' actionformats '%F{white}(%F{cyan}%b %F{reset}|%F{green} √ %F{reset}%F{white})%F{reset}'
-zstyle ':vcs_info:git:*' formats '%F{white}(%F{cyan}%b %F{reset}|%F{red} ✗%F{reset}%F{white})%F{reset}'
+zstyle ':vcs_info:git:*' actionformats '%F{white}(%F{cyan}%b%F{white})%F{reset}'
+zstyle ':vcs_info:git:*' formats '%F{white}(%F{cyan}%b %F{reset}|%F{red} ✗ %F{reset}%F{white})%F{reset}'
 zstyle ':vcs_info:*' enable git
 precmd() { vcs_info }
 setopt PROMPT_SUBST
 PROMPT=$'\n%F{yellow}%~ ${vcs_info_msg_1_}${vcs_info_msg_0_}%F{reset}\n $ '
 
+zstyle ':vcs_info:*' check-for-changes true
++vi-git() {
+    if (( $#BUFFER == 0 )); then
+        git diff --quiet --ignore-submodules HEAD &>/dev/null
+        [[ $? == 0 ]] && hook_com[staged]=''
+    fi
+}
+zstyle -e ':vcs_info:git+set-message:*' hooks 'reply=(vi-git)'
