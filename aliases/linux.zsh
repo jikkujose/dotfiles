e95f0ae277037037b5fd31adabde023bd9e6252a
Jikku Jose <jikku@storybrain.com>
2023-08-02 02:56:44 +0530

    Savepoint

--- aliases/linux.zsh
+++ aliases/linux.zsh
@@ -2,4 +2,4 @@ export DISPLAY=:1
 
 alias c='xclip -selection clipboard'
 alias p='xclip -o -selection clipboard'
-alias llama2="python server.py --load-in-8bit"
+alias tg-webui="python server.py --load-in-8bit"
