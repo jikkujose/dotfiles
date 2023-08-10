6dfd5a0027dc7a69164cd029cc31c16fd128f08a
Jikku Jose <jikku@storybrain.com>
2023-08-10 20:28:32 +0530

    Update tg-webui

--- aliases/linux.zsh
+++ aliases/linux.zsh
@@ -2,5 +2,5 @@ export DISPLAY=:1
 
 alias c='xclip -selection clipboard'
 alias p='xclip -o -selection clipboard'
-alias tg-webui="python server.py --load-in-8bit"
+alias tg-webui="python server.py --load-in-8bit --listen"
 alias ip="hostname -I"
