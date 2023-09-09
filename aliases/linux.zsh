a68836afc7f8b31b537f93a5556ba5c06d637938
Jikku Jose <jikku@storybrain.com>
2023-09-09 10:06:42 +0530

    Update ip

--- aliases/linux.zsh
+++ aliases/linux.zsh
@@ -3,4 +3,4 @@ export DISPLAY=:1
 alias c='xclip -selection clipboard'
 alias p='xclip -o -selection clipboard'
 alias tg-webui="python server.py --load-in-8bit --listen"
-alias ip="hostname -I"
+alias ip="hostname -I | rl 'split[1]'"
