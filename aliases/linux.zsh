a2365071c1cde16bd8cf4bd1e031d6c112ae38bd
Jikku Jose <jikku@storybrain.com>
2023-09-28 02:54:38 +0530

    Add square-one-init

--- aliases/linux.zsh
+++ aliases/linux.zsh
@@ -4,3 +4,4 @@ alias c='xclip -selection clipboard'
 alias p='xclip -o -selection clipboard'
 alias tg-webui="python server.py --load-in-8bit --listen"
 alias ip="hostname -I | rl 'split[1]'"
+alias squareone-init="cp -r ~/Projects/square-one/* ."
