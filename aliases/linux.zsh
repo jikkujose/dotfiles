8897693e7bd36c570182f2cdbb7e58b4af393157
Jikku Jose <jikku@storybrain.com>
2023-10-05 18:36:17 +0530

    Update to rapid

--- aliases/linux.zsh
+++ aliases/linux.zsh
@@ -4,4 +4,4 @@ alias c='xclip -selection clipboard'
 alias p='xclip -o -selection clipboard'
 alias tg-webui="python server.py --load-in-8bit --listen"
 alias ip="hostname -I | rl 'split[1]'"
-alias squareone-init="cp -r ~/Projects/square-one/* ."
+alias rapid="cp -r ~/Projects/rapid/boilerplate/* ."
