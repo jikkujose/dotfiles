9c7f1dd573964180a7db1e48ed3c40acad20ed16
Jikku Jose <jikku@storybrain.com>
2024-04-20 01:42:59 +0530

    Add cuda path

--- zshrc/linux.zsh
+++ zshrc/linux.zsh
@@ -4,4 +4,10 @@ fpath=(${ASDF_DIR}/completions $fpath)
 autoload -Uz compinit && compinit
 
 export HOST_IP="$(ip route |awk '/^default/{print $3}')"
+
 export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
+
+# Cuda
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
