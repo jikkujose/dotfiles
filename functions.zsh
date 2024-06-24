403c8bf3aa5650a46464403c1ad2112660d98150
Jikku Jose <jikku@storybrain.com>
2024-06-24 19:33:34 +0530

    Add expose

--- functions.zsh
+++ functions.zsh
@@ -27,3 +27,12 @@ q () {
 qq () {
   xclip -o -selection clipboard | llm -i li -p json | tabularize
 }
+
+function expose() {
+  if [[ -z "$1" ]]; then
+    echo "Error: No port specified."
+    return 1  # Exit the function with an error status
+  fi
+  local port=$1
+  ssh -R 80:localhost:${port} nokey@localhost.run 2>&1 | awk '/https:\/\/[a-zA-Z0-9]+\.lhr\.life/ {print $NF; fflush(); exit}'
+}
