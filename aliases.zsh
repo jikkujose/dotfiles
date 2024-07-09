76263cb8d402cbb4c840596bce42db6e3389b052
Jikku Jose <jikku@storybrain.com>
2024-07-09 11:46:41 +0530

    Add v, unstage & update yy

--- aliases.zsh
+++ aliases.zsh
@@ -62,7 +62,7 @@ alias nm_delete="find . -name 'node_modules' -type d -prune -print0 | xargs -0 r
 
 alias tb="nc termbin.com 9999"
 alias city="curl https://am.i.mullvad.net/city"
-alias yy="yt-dlp -f 'bestvideo[ext=mp4][vcodec=avc1]+bestaudio[ext=m4a][acodec=mp4a]/best[ext=mp4][vcodec=avc1]/best'"
+alias yy="yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]' -S vcodec:h264"
 alias br="bun run"
 alias python="python3"
 alias lora="~/Projects/gpt4all/chat/gpt4all-lora-quantized-linux-x86  -m ~/Projects/gpt4all/chat/gpt4all-lora-unfiltered-quantized.bin"
@@ -70,3 +70,5 @@ alias server="ruby -rwebrick -e 'port = ARGV[0] || 8000; WEBrick::HTTPServer.new
 
 alias pp="xargs -0 echo -e"
 alias gallery="npx gallery-server --no-footer -p 2323 -t 2323 --folder ."
+alias v="cvlc"
+alias unstage="git reset --"
