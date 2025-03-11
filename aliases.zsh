6b454a96db5d9d59a3296e34392de232bc021b04
Jikku Jose <jikku@storybrain.com>
2025-03-11 15:22:14 +0530

    Add unix timestamp

--- aliases.zsh
+++ aliases.zsh
@@ -50,6 +50,8 @@ alias rrr="cd ~/Ruby/scrap"
 alias rm="rm -i"
 alias rmrf="rm -rfi"
 
+alias uu="date +%s"
+
 alias pf="ps aux | ag"
 alias sl="df -h / | tail -n 1 | ruby -e \"puts STDIN.read.split(' ')[3].split('G')[0] + ' GB'\""
 alias ta="tmux attach"
