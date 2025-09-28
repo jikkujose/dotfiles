b2a4e6be9a695bc76c7562ebfc1ad11b28885481
Jikku Jose <jikku@storybrain.com>
2025-09-29 00:37:22 +0400

    Update

--- zshrc/mac.zsh
+++ zshrc/mac.zsh
@@ -1,2 +1,3 @@
-source `brew --prefix`/etc/profile.d/z.sh
-. "/usr/local/opt/asdf/libexec/asdf.sh"
+# Replace content in ~/dotfiles/zshrc/mac.zsh with:
+source $(brew --prefix)/share/zsh-site-functions/_z 2>/dev/null || source $(brew --prefix)/etc/profile.d/z.sh 2>/dev/null
+source $(brew --prefix asdf)/libexec/asdf.sh 2>/dev/null
