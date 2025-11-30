4e07bf411720b7f055ef64b6d953a9fc9f434e1a
Jikku Jose <jikku@storybrain.com>
2025-11-30 10:21:02 +0400

    Update with python server

--- aliases.zsh
+++ aliases.zsh
@@ -69,7 +69,7 @@ alias ys="yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=mp4]' -S vcodec:h264"
 alias br="bun run"
 alias python="python3"
 alias lora="~/Projects/gpt4all/chat/gpt4all-lora-quantized-linux-x86  -m ~/Projects/gpt4all/chat/gpt4all-lora-unfiltered-quantized.bin"
-alias server="ruby -rwebrick -e 'port = ARGV[0] || 8000; WEBrick::HTTPServer.new(:Port => port.to_i, :DocumentRoot => Dir.pwd).start' --"
+alias server='python3 -m http.server'
 alias docker="podman"
 
 alias pp="xargs -0 echo -e"
